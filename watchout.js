7012cc7850b54ff95f65b58737b528320c53336c
MKS-students <mks.students@hackreactor.com>
2015-06-19 19:27:01 -0500

    Moving Player

--- watchout.js
+++ watchout.js
@@ -6,17 +6,23 @@
   //ease the animation
   //repeat in one second
 
-var createEnemy = function() {
-  return d3.select('body').selectAll('.enemy').data([1,2,3,4,5,6]).enter()
-   .append("svg").attr({"width": 50,"height": 50, "class": "enemy"})
-   .append("circle").attr({"cx": 25,"cy": 25,"r": 25}).style("fill", "purple")
-}
-var moveEnemy = function() { return d3.select('body').selectAll('.enemy').transition().style(
-  {'position' : 'absolute',
-    'top': function(){
-    return d3.select('body').node().getBoundingClientRect().height * Math.random();
-  }, 'left': function(){
-    return d3.select('body').node().getBoundingClientRect().width * Math.random();
+var box = d3.select('body')
+              .append('svg')
+              .attr('class', 'box')
+              .attr('width', 700)
+              .attr('height', 700);
+
+var createEnemy = d3.select('.box').selectAll('.enemy').data([1,2,3,4,5,6]).enter()
+   .append("svg").attr({"width": 700,"height": 700, "class": "enemy"})
+   .append("circle").attr({"cx": 25,"cy": 25,"r": 25}).style({"fill": "purple",'position' : 'absolute'})
+
+var moveEnemy = function() { return d3.select('.box').selectAll('.enemy').selectAll('circle').transition().attr(
+  {
+    'cy': function(){
+      debugger;
+    return d3.select('.box').node().getBoundingClientRect().height * Math.random();
+  }, 'cx': function(){
+    return d3.select('.box').node().getBoundingClientRect().width * Math.random();
   }
 })}
 
@@ -26,13 +32,17 @@ var movingEnemies = function() {
     movingEnemies();
   }, 1000)
 }
+var dragon = d3.behavior.drag()
+.on('dragstart', function(){createPlayer.style({'fill':'blue'})})
+.on('drag', function(){createPlayer.attr('cx', d3.event.x).attr('cy', d3.event.y)})
+.on('dragend', function(){createPlayer.style({'fill':'blue'})});
+//
+var createPlayer = d3.select('.box')
+   .append("svg").attr({"width": 700,"height": 700, "class": "player"})
+   .append("circle").style({"fill": "red", 'position' : 'absolute'})
+   .attr({"cx": 25,"cy": 25,"r": 25})
 
-var createPlayer = function() {
-  return d3.select('body').selectAll('.player').data([1]).enter()
-   .append("svg").attr({"width": 50,"height": 50, "class": "player"})
-   .append("circle").attr({"cx": 25,"cy": 25,"r": 25}).style("fill", "red")
-}
+d3.select('.player').call(dragon);
 
-
-createEnemy()
-movingEnemies();
+moveEnemy();
+movingEnemies()
