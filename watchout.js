e2132713e88853457eeef48cad523d7c78dc9540
MKS-students <mks.students@hackreactor.com>
2015-06-20 16:12:24 -0500

    Collisions Fixed

--- watchout.js
+++ watchout.js
@@ -6,7 +6,7 @@ var box = d3.select('body')
               .append('svg')
               .attr({'class': 'box','width': 700,'height': 700, 'border': '10px solid red'});
 
-var createEnemy = d3.select('.box').selectAll('.enemy').data([1,2,3,4,5,6]).enter()
+var createEnemy = d3.select('.box').selectAll('.enemy').data(d3.range(Math.random()*20)).enter()
    .append("svg").attr({"width": 700,"height": 700, "class": "enemy"})
    .append("circle").attr({"cx": 25,"cy": 25,"r": 25}).style({"fill": "purple",'position' : 'absolute'})
 
@@ -38,29 +38,31 @@ var createPlayer = d3.select('.box')
 
 d3.select('.player').call(dragon);
 
+var prevCollision = false;
 
-var collision = function() {
-  setInterval(function(){
-    var playerX = Math.abs(d3.select('.player').select('circle').attr('cx'))
-    var playerY = Math.abs(d3.select('.player').select('circle').attr('cy'))
-    var playerR = Math.abs(d3.select('.player').select('circle').attr('r'))
+var checkCollision = function() {
+  var playerX = Math.abs(d3.select('.player').select('circle').attr('cx'))
+  var playerY = Math.abs(d3.select('.player').select('circle').attr('cy'))
+  var playerR = Math.abs(d3.select('.player').select('circle').attr('r'))
 
-    var checkCollision = function() {
-      for (var i = 0; i < createEnemy[0].length; i++){
-        var enemy = createEnemy[0][i];
-        var enemyX = enemy.cx.animVal.value
-        var enemyY = enemy.cy.animVal.value
-        var enemyR = enemy.r.animVal.value
-        if (Math.abs(enemyX-playerX) < 50 && Math.abs(enemyY-playerY) < 50) {
-          collisionCount++
-          score=0;
-        }
-      }
+
+  var collision = false;
+
+  for (var i = 0; i < createEnemy[0].length; i++){
+    var enemy = createEnemy[0][i];
+    var enemyX = enemy.cx.animVal.value
+    var enemyY = enemy.cy.animVal.value
+    var enemyR = enemy.r.animVal.value
+    if (Math.abs(enemyX-playerX) < 50 && Math.abs(enemyY-playerY) < 50) {
+      collision = true;
+      score = 0
     }
-    checkCollision()
-  },100)
+  }
+  if (collision !== prevCollision) {
+    collisionCount++
+  }
+  prevCollision=collision
 }
-
 var updateScore = function() {
   d3.select('.scoreboard .current span').text(score)
   d3.select('.scoreboard .high span').text(highScore)
@@ -68,12 +70,12 @@ var updateScore = function() {
 }
 var increaseScore = function() {
   setInterval(function() {
-    score++
+    score++;
     highScore = Math.max(score, highScore)
     updateScore()
   }, 100)
 }
 
-collision()
+setInterval(checkCollision, 100);
 movingEnemies()
 increaseScore()
